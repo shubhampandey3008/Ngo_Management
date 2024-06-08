c465022898500356f7b7e2377e38b5d339e67101
shubham pandey <shubhampaney3007@gmail.com>
2024-06-08 23:12:30 +0530

    Completed navbar and added Carousel including one picture

--- Backend/backend/app/page.tsx
+++ Backend/backend/app/page.tsx
@@ -1,8 +1,23 @@
 import Image from "next/image";
 import Navbar from './components/navbar/Navbar';
+import EmblaCarousel from './components/Carousel/EmblaCarousel';
+import { EmblaOptionsType } from 'embla-carousel'
+
+const IMAGE_SRC = "https://uttarakhandtourism.gov.in/sites/default/files/2020-07/Pithoragarh%20Banner1.jpg"
+const OPTIONS: EmblaOptionsType = { loop: true }
+const SLIDE_COUNT = 5
+const images = Array.from({length : SLIDE_COUNT} , (_ , index) => {
+  <img
+  key={index}
+  src = {IMAGE_SRC}
+  alt = {` Image ${index + 1}`}></img>
+})
 
 export default function Home() {
   return (
+     <>
      <Navbar />
+     <EmblaCarousel slides={images} options={OPTIONS} />
+     </>
   )
 }
