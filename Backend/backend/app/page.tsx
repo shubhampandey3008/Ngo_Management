46dbb0ba44f38ed4204db01ef1c30a70f18ed54e
shubham pandey <shubhampaney3007@gmail.com>
2024-06-09 09:43:37 +0530

    Added autoplay and corrected the errors

--- Backend/backend/app/page.tsx
+++ Backend/backend/app/page.tsx
@@ -3,21 +3,16 @@ import Navbar from './components/navbar/Navbar';
 import EmblaCarousel from './components/Carousel/EmblaCarousel';
 import { EmblaOptionsType } from 'embla-carousel'
 
-const IMAGE_SRC = "https://uttarakhandtourism.gov.in/sites/default/files/2020-07/Pithoragarh%20Banner1.jpg"
 const OPTIONS: EmblaOptionsType = { loop: true }
 const SLIDE_COUNT = 5
-const images = Array.from({length : SLIDE_COUNT} , (_ , index) => {
-  <img
-  key={index}
-  src = {IMAGE_SRC}
-  alt = {` Image ${index + 1}`}></img>
-})
+
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
 export default function Home() {
   return (
      <>
      <Navbar />
-     <EmblaCarousel slides={images} options={OPTIONS} />
+     <EmblaCarousel slides={SLIDES} options={OPTIONS} />
      </>
   )
 }
