5d0290cb99163e9b4f1da928b08a6dea451d4210
Shubham Pandey <shubhampaney3007@gmail.com>
2024-07-09 23:43:52 +0530

    Made the table dynamic

--- Website/Client/app/students/page.tsx
+++ Website/Client/app/students/page.tsx
@@ -1,7 +1,16 @@
 import StudentTable from "../components/tableTemplate/tableTemplate";
+import { Student_Study } from "./student_study";
+
+async function getStudentStudy(): Promise<Student_Study[]> {
+  const res = await fetch("");
+  const data = await res.json();
+  return data;
+}
 
 // `app/page.tsx` is the UI for the `/` URL
-export default function Student() {
+export default async function StudentPage() {
+  const student_study = await getStudentStudy();
+
   return (
     <>
       <div className="border-4 border-green-200 flex">
@@ -17,7 +26,7 @@ export default function Student() {
           </div>
         </div>
         <div className="border-4 border-blue-200 m-5 w-2/3">
-          <StudentTable />
+          <StudentTable student_study={student_study} />
         </div>
       </div>
     </>
---
+++ Website/Client/app/students/student_info.tsx
@@ -0,0 +1,8 @@
+export type Student = {
+  Id: string;
+  Name: string;
+  Photo: string;
+  Class: string;
+  DateOfJoining: Date;
+  LastPresent: Date;
+};
