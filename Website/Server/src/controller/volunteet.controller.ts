27da74ec7e55a2ebfa79a38888453e120a541156
shubham pandey <shubhampaney3007@gmail.com>
2024-09-05 17:22:50 +0530

    Created Volunteer API

---
+++ Website/Server/src/controller/volunteet.controller.ts
@@ -0,0 +1,38 @@
+import { Request , Response } from "express";
+import { VolunteerModel } from "../models/volunteer.model";
+import { Code } from "../enum/Code";
+import { HttpResponse } from "../domain/response";
+import { Status } from "../enum/Status";
+
+export async function createVolunteer(res : Response , req : Request)
+{
+    try {
+        await VolunteerModel.create(req.body);
+        res.status(Code.OK).json(new HttpResponse(
+            Code.OK,
+            Status.OK,
+            "Request Completed"
+        ))
+    } catch (error) {
+        res.status(Code.INTERNAL_SERVER_ERROR).json(new HttpResponse(
+            Code.INTERNAL_SERVER_ERROR,
+            Status.INTERNAL_SERVER_ERROR,
+            "Request was not completed"
+        ));
+    }
+}
+
+export async function getVolunteer(req : Request , res : Response)
+{
+    try {
+        const volunteerId = req.query.id;
+        const volunteer = await VolunteerModel.findById(volunteerId);
+        res.status(Code.OK).json(volunteer)
+    } catch (error) {
+        res.status(Code.INTERNAL_SERVER_ERROR).json(new HttpResponse(
+            Code.INTERNAL_SERVER_ERROR,
+            Status.INTERNAL_SERVER_ERROR,
+            "Request was not completed"
+        ));
+    }
+}
---
+++ Website/Server/src/models/volunteer.model.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const Volunteer_Details = new mongoose.Schema({
+    date: Date,
+    attendance: String,
+    students: [String]
+});
+
+const VolunteerSchema = new mongoose.Schema({
+    name: String,
+    phone_no: Number,
+    email: String,
+    doj: Date,
+    weekend_details : [Volunteer_Details]
+},
+{
+    timestamps : true
+});
+
+export const VolunteerModel = mongoose.model("Volunteer" , VolunteerSchema);
